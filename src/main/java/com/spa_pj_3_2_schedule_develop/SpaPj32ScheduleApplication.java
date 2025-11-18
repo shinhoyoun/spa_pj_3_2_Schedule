0c4eb230e67850f814191826dd2a043637f9561b
SHY <shy9729@gmail.com>
2025-11-18 12:29:23 +0900

    create

---
+++ src/main/java/com/spa_pj_3_2_schedule_develop/SpaPj32ScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.spa_pj_3_2_schedule_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaPj32ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaPj32ScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spa_pj_3_2_schedule_develop/SpaPj32ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa_pj_3_2_schedule_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaPj32ScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
